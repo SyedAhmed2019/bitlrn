778b025ad8e029a72c020f5aae7873164665232b
Basheeruddin <basheeruddin@Users-MacBook-Pro.local>
2022-05-04 11:00:48 -0700

    14430504 1.BinarySearch 2.OddEvenCounter
    Signed-off-by: Basheeruddin <basheeruddin@Users-MacBook-Pro.local>

---
+++ samples/src/main/java/com/bitlrn/beginner/BinarySearch.java
@@ -0,0 +1,93 @@
+package com.bitlrn.beginner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public int findElementNonRecursive(int[] input,int element){
+        int low = 0;
+        int high = input.length-1;
+        while ( low < high){
+            if (element == input[low]){
+                return low;
+            }
+            if (element == input[high]){
+                return high;
+            }
+            int mid = (low+high)/2;
+            if ( element > input[mid]){
+                low = mid+1;
+            }else if ( element == input[mid]){
+                return mid;
+            }else{
+                high = mid;
+            }
+        }
+        return -1;
+    }
+    public int findElementRecursive(int[] input, int min, int max, int elem){
+        if (min >= max){
+            return -1;
+        }
+        // are we lucky with min
+        if (elem == input[min]){
+            return min;
+        }
+        //are we lucky with max
+        if (elem == input[max]){
+            return max;
+        }
+        // is it in middle to max side
+        if (elem > input[(min+max)/2]){
+            return findElementRecursive(input,(min+max)/2+1,max,elem);
+        }else{
+            // is it in min to middle
+            return findElementRecursive(input, min, (min+max)/2,elem);
+        }
+    }
+
+    public static void main(String[] args){
+        nonUserInput();
+        userInput();
+    }
+
+    private static void userInput() {
+        BinarySearch bis = new BinarySearch();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of input array:");
+        int inputSize = scanner.nextInt();
+        int []inputArray = new int[inputSize];
+        int i=0;
+        System.out.println("Enter the sorted elements of input array ");
+        while (i < inputSize){
+            System.out.println("Enter "+ i + "element of input array:");
+            inputArray[i++]=scanner.nextInt();
+        }
+        System.out.println("Enter the element to find in array:");
+        int elem = scanner.nextInt();
+        int pos = bis.findElementRecursive(inputArray,0,inputArray.length-1,elem);
+        if (pos == -1){
+            System.out.println("Element "+ elem + " was not found using recursive method");
+        }else{
+            System.out.println("Element "+elem+ " was found at position "+ pos +" in input array using recursive method");
+        }
+        pos = bis.findElementNonRecursive(inputArray,elem);
+        if (pos == -1){
+            System.out.println("Element "+ elem + " was not found using non-recursive method");
+        }else{
+            System.out.println("Element "+elem+ " was found at position "+ pos +" in input array using non-recursive method");
+        }
+    }
+
+    private static void nonUserInput() {
+        BinarySearch bis = new BinarySearch();
+        int[][] inputLists = {{}, {0, 1}, {1, 2, 3}, {-2, 0, 3, 5, 8, 15}, {-19, 0, 3, 5, 9, 15}};
+        int[] lookupList = {12, 1, 3, 8, 2};
+        for (int i=0; i <inputLists.length;++i){
+            System.out.println("input list :"+ Arrays.toString(inputLists[i]));
+            System.out.println("find element :"+lookupList[i]);
+            System.out.println("result non-recursive:"+bis.findElementNonRecursive(inputLists[i],lookupList[i]));
+            System.out.println("result recursive:"+ bis.findElementRecursive(inputLists[i],0,inputLists[i].length-1,lookupList[i]));
+        }
+    }
+}
---
+++ samples/src/test/java/com/bitlrn/beginner/BinarySearchTest.java
@@ -0,0 +1,61 @@
+package com.bitlrn.beginner;
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+    @Test
+    void findElement_found_in_array_thenPass() {
+        BinarySearch bis = new BinarySearch();
+        int []input = new int[]{1,2,3,4,5};
+        int pos = bis.findElementRecursive(input,0,4, 4);
+        assert pos==3;
+        pos = bis.findElementNonRecursive(input,4);
+        assert pos==3;
+        input = new int[]{1,2,3,4,5,6};
+        pos = bis.findElementRecursive(input,0,5, 4);
+        assert pos==3;
+        pos = bis.findElementNonRecursive(input,4);
+        assert pos==3;
+    }
+
+    @Test
+    void findElement_not_found_in_array_thenPass() {
+        BinarySearch bis = new BinarySearch();
+        int []input = new int[]{1,2,3,4,5};
+        int pos = bis.findElementRecursive(input,0,4, 6);
+        assert pos==-1;
+        pos = bis.findElementNonRecursive(input,6);
+        assert pos==-1;
+        input = new int[]{1,2,3,4,5,6};
+        pos = bis.findElementRecursive(input,0,5, 0);
+        assert pos==-1;
+        pos = bis.findElementNonRecursive(input,0);
+        assert pos==-1;
+    }
+
+    @Test
+    void findElement_found_at_0_pos_or_max_pos_thenPass() {
+        BinarySearch bis = new BinarySearch();
+        int []input = new int[]{1,2,3,4,5};
+        int pos = bis.findElementRecursive(input,0,4, 1);
+        assert pos==0;
+        pos = bis.findElementNonRecursive(input,1);
+        assert pos==0;
+        input = new int[]{1,2,3,4,5,6};
+        pos = bis.findElementRecursive(input,0,5, 6);
+        assert pos==5;
+        pos = bis.findElementNonRecursive(input,6);
+        assert pos==5;
+
+    }
+
+    @Test
+    void findElement_in_empty_array_thenPass() {
+        BinarySearch bis = new BinarySearch();
+        int []input = new int[]{};
+        int pos = bis.findElementRecursive(input,0,input.length-1, 4);
+        assert pos==-1;
+        pos = bis.findElementNonRecursive(input,4);
+        assert pos==-1;
+    }
+}
